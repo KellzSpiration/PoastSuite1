f2e7be5de8ba478cadcf0d515c400513f3f7c8e2
Kellz <kaycekayceology007@gmail.com>
2021-08-12 06:21:17 +0100

    latest update

---
+++ PoastVOne/PoastVOne/QuestionEight/GradePageQ8.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PoastVOne.QuestionEight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GradePageQ8 : ContentPage
+    {
+        private double s;
+        public GradePageQ8(double score5)
+        {
+            InitializeComponent();
+            s = score5;
+        }
+
+       async private void ExitHJModule_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new FirstIterationQ8());
+        }
+
+        private void ReviewAndGrade_Clicked(object sender, EventArgs e)
+        {
+            UserScore.Text = s + "%".ToString();
+        }
+    }
+}
---
+++ PoastVOne/PoastVOne/QuestionSeven/GradePageQ7.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PoastVOne.QuestionSeven
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GradePageQ7 : ContentPage
+    {
+        private double s;
+        public GradePageQ7(double score5)
+        {
+            InitializeComponent();
+            s = score5;
+        }
+
+      async  private void ExitHJModule_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new FirstIterationQ7());
+        }
+
+        private void ReviewAndGrade_Clicked(object sender, EventArgs e)
+        {
+            UserScore.Text = s + "%".ToString();
+        }
+    }
+}
